fa5482238142c8da3f1295c119f4fe7a47f801d5
andrea.mancuso <andrea.mancuso@u-blox.com>
2024-07-21 13:55:57 +0200

    Update CMakeLists.txt and tiledownloader.cpp

--- cpp/tiledownloader.cpp
+++ cpp/tiledownloader.cpp
@@ -5,30 +5,6 @@
 #include "tiledownloader.h"
 #include "shared.h"
 
-void downloadSucceeded(emscripten_fetch_t *fetch) {
-    printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes, fetch->url);
-  
-    auto tileDescriptor = reinterpret_cast<TileDescriptor*>(fetch->userData);
-    
-    printf("About to mark event as set\n");
-
-    tileDescriptor->HandleSuccess(fetch);
-
-    printf("Marked event as set\n");
-
-    emscripten_fetch_close(fetch); // Free data associated with the fetch.
-}
-
-void downloadFailed(emscripten_fetch_t *fetch) {
-    printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url, fetch->status);
-
-    auto tileDescriptor = reinterpret_cast<TileDescriptor*>(fetch->userData);
-
-    tileDescriptor->HandleFailure(fetch);
-
-    emscripten_fetch_close(fetch); // Also free data on failure.
-}
-
 void download(TileDescriptor& tileDescriptor) {
     emscripten_fetch_attr_t attr;
     emscripten_fetch_attr_init(&attr);
@@ -37,8 +13,29 @@ void download(TileDescriptor& tileDescriptor) {
     attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
 
     attr.userData = (void*)&tileDescriptor;
-    attr.onsuccess = downloadSucceeded;
-    attr.onerror = downloadFailed;
+    attr.onsuccess = [](emscripten_fetch_t *fetch) {
+        printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes, fetch->url);
+    
+        auto tileDescriptor = reinterpret_cast<TileDescriptor*>(fetch->userData);
+        
+        printf("About to mark event as set\n");
+
+        tileDescriptor->HandleSuccess(fetch);
+
+        printf("Marked event as set\n");
+
+        emscripten_fetch_close(fetch); // Free data associated with the fetch.
+    };
+
+    attr.onerror = [](emscripten_fetch_t *fetch) {
+        printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url, fetch->status);
+
+        auto tileDescriptor = reinterpret_cast<TileDescriptor*>(fetch->userData);
+
+        tileDescriptor->HandleFailure(fetch);
+
+        emscripten_fetch_close(fetch); // Also free data on failure.
+    };
 
     // printf("Inside promise d %s\n", tileDescriptor.url.c_str());
 
