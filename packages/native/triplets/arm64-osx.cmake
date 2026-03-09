65c519da9177f2ac8563bb76b051a5121538b87a
Andrea Mancuso <and.mancuso@googlemail.com>
2026-03-09 21:42:05 +0100

    Another attempt at reducing the build size for linux and mac

--- packages/native/triplets/arm64-osx.cmake
+++ packages/native/triplets/arm64-osx.cmake
@@ -4,7 +4,3 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_OSX_DEPLOYMENT_TARGET "15.0")
-
-# Enable per-function/data sections so -dead_strip can strip unused code from vcpkg libs
-set(VCPKG_C_FLAGS "-ffunction-sections -fdata-sections")
-set(VCPKG_CXX_FLAGS "-ffunction-sections -fdata-sections")
